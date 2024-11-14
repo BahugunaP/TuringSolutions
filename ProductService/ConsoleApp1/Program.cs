8ee5ce927c437946e4d3d685fd126d0985926c63
Pankaj Bahuguna <pankaj.bahuguna@clickminds.com>
2024-11-14 16:30:18 +0530

    Added Ocelot Solution

---
+++ ProductService/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+
+namespace Client.APICalling
+{
+    class APICalling
+    {
+        static void Main()
+        {
+            ProductClient productClient = new ProductClient(new HttpClient());
+            string response = productClient.GetProductsAsync().Result;
+        }
+    }
+}
+public class ProductClient
+{
+    private readonly HttpClient _httpClient;
+
+    public ProductClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public
+ async Task<string> GetProductsAsync()
+    {
+        var response = await _httpClient.GetAsync("http://localhost:5096//products");
+        return response.ToString();
+    }
+}

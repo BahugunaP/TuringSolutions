b3a8d65be63ca55c8fb609ebfbe59d41933af904
Pankaj Bahuguna <pankaj.bahuguna@clickminds.com>
2024-11-15 13:26:43 +0530

    Added Delayes in Code Execution

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AsyncAwait;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Task<string> dataTask = GetDataFromApiAsync();
+
+        // Continue with other work while waiting for the data
+        Console.WriteLine("Doing other work...");
+
+        // Wait for the data to become available
+        string data = await dataTask;
+
+        // Process the data
+        Console.WriteLine($"Received data: {data}");
+    }
+    static async Task<string> GetDataFromApiAsync()
+    {
+        using (var client = new HttpClient())
+        {
+            // Make the API request
+            HttpResponseMessage response = await client.GetAsync("http://localhost:5096//products");
+
+            //Added Delayes In Ececution  
+            await Task.Delay(1000);
+            // Check the response status code
+            response.EnsureSuccessStatusCode();
+
+            // Read the response content
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
